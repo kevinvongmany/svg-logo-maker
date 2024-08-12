3dba10423e3068622cc4612cb4dccd466b295250
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-08-12 18:18:48 +1000

    Added prompts and SVG generator logic

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const generator = require('./lib/generator');
+const inquirer = require('inquirer');
 
-module.exports = {
-    // Exported functions, variables, or objects
-};
+const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Which shape would you like to render?',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'colour',
+        message: 'Enter the colour of the shape:'
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter 3 characters to have inside the shape:'
+    },
+    {
+        type: 'input',
+        name: 'textColour',
+        message: 'Enter the colour of the text:'
+    }
+];
+
+
+const generateText = (text, xPos, yPos, fontsize, colour) => {
+    return `<text x="${xPos}" y="${yPos}" font-size="${fontsize}" fill="${colour}" text-anchor="middle">${text}</text>`;
+}
+
+inquirer.prompt(questions).then(answers => {
+    let shape, text;
+    switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle();
+            text = generateText(answers.text, 150, 120, 50, answers.textColour);
+            break;
+        case 'Square':
+            shape = new Square();
+            text = generateText(answers.text, 150, 145, 50, answers.textColour);
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            text = generateText(answers.text, 150, 150, 40, answers.textColour);
+            break;
+    }
+    shape.setColour(answers.colour);
+    const svg = new generator(shape.render(), text);
+    svg.saveToFile(answers.shape.toLowerCase());
+    console.log('SVG file created!');
+});
